c35278d26513568b67d89c50cc7ea21389ae793c
Wycliffe Ochieng <KEN20198@kcbgroup.com>
2021-07-14 17:41:31 +0300

    Fixed imports usage

--- src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
+++ src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
@@ -2,7 +2,6 @@ package com.wybosoft.microservices.limits.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wybosoft.microservices.limits.Configuration;
