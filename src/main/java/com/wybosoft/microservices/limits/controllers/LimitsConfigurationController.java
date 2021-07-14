75e2c91dfebb22b9b8e8d985b39f4e802e6cfc34
Wycliffe Ochieng <KEN20198@kcbgroup.com>
2021-07-14 17:40:56 +0300

    added Config controller

--- src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
+++ src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
@@ -1,20 +1,27 @@
 package com.wybosoft.microservices.limits.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wybosoft.microservices.limits.Configuration;
 import com.wybosoft.microservices.limits.model.LimitConfiguration;
 
 @RestController
 //@RequestMapping(value="/limits")
 
+
 public class LimitsConfigurationController {
+	
+	@Autowired
+	private Configuration configuration;
+
 	@GetMapping(value="/limits")
 	
 	public LimitConfiguration retriveLimitsFromConfigurations()  
 	{  
-	return new LimitConfiguration(1000, 1);  
+	return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
 	}  
 } //end class
 
