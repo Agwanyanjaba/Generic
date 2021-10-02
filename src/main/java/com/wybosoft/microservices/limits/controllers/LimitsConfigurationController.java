8facf6063a47de737d89278e0ac150753b851b35
bonkewycliffe@yahoo.com <bonkewycliffe@yahoo.com>
2021-10-02 22:25:48 +0300

    Updated java version -Limits Service

--- src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
+++ src/main/java/com/wybosoft/microservices/limits/controllers/LimitsConfigurationController.java
@@ -2,13 +2,14 @@ package com.wybosoft.microservices.limits.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wybosoft.microservices.limits.Configuration;
 import com.wybosoft.microservices.limits.model.LimitConfiguration;
 
 @RestController
-//@RequestMapping(value="/limits")
+@RequestMapping(value="/limits/1.0.0")
 
 
 public class LimitsConfigurationController {
@@ -19,6 +20,13 @@ public class LimitsConfigurationController {
 	@GetMapping(value="/limits")
 	
 	public LimitConfiguration retriveLimitsFromConfigurations()  
+	{  
+	return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
+	}  
+	
+@GetMapping(value="/limit-check")
+	
+	public LimitConfiguration retriveLimitsFromConfigurations1()  
 	{  
 	return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());  
 	}  
